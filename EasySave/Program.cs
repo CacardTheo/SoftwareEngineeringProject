23f0c76aad1e47f2bd3a7c9cc177ab84c99f2763
Théo Cacard <cacardt@iut-acy.univ-smb.fr>
2026-04-23 00:12:40 +0200

    Change of file architecture

---
+++ EasySave/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SoftwareEngineeringProject;
+
+namespace SoftwareEngineeringProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                ConsoleView view = new ConsoleView();
+                view.Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"A critical error occurred: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
